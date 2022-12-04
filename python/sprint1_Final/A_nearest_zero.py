334820adb522250bfefbd213919af5574f8cfffb
Bodra84 <Bodra84@mail.ru>
2022-12-04 16:53:16 +0300

    Спринт 1 ревью 01

--- python/sprint1_Final/A_nearest_zero.py
+++ python/sprint1_Final/A_nearest_zero.py
@@ -1,10 +1,10 @@
-#ID 78411394 4 дек 2022, 10:57:32
+#ID 78639100 4 дек 2022, 16:36:55
 def nearest_zero(n, data):
     """Функция вычисления ближайщего нуля в последовательности data."""
     count = n
     result = []
-    for i in range(n):
-        if data[i] == 0:
+    for num in data:
+        if num == 0:
             result.append(0)
             count = 1
         else:
@@ -24,13 +24,13 @@ def nearest_zero(n, data):
 def read_input():
     """Функция ввода исходных данных."""
     n = int(input())
-    data = list(map(int, input().strip().split()))
+    data = [int(i) for i in input().split()]
     return n, data
 
 
 def main():
     n, data = read_input()
-    print(" ".join(map(str, nearest_zero(n, data))))
+    print(*nearest_zero(n, data))
 
 
 if __name__ == '__main__':
--- python/sprint1_nonfinals/e.py
+++ python/sprint1_nonfinals/e.py
@@ -1,14 +1,26 @@
+import re
+
+
 def get_longest_word(line: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    result = ()
+    line = re.findall(r'\S+', line)
+    maxlen = max(len(word) for word in line)
+    for word in line:
+        if len(word) == maxlen:
+            result = word
+            break
+    return result
+
 
 def read_input() -> str:
     _ = input()
     line = input().strip()
     return line
 
+
 def print_result(result: str) -> None:
     print(result)
     print(len(result))
+
 
 print_result(get_longest_word(read_input()))
