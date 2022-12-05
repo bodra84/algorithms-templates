fe7d6d0a3c6c78a4a3617c0ca91325e18e2dd2a1
Bodra84 <Bodra84@mail.ru>
2022-12-05 21:16:22 +0300

    Задача Палиндром

--- python/sprint1_nonfinals/f.py
+++ python/sprint1_nonfinals/f.py
@@ -1,5 +1,14 @@
+import re
+
+
 def is_palindrome(line: str) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    line = line.lower()
+    line = re.split(r'\W+', line)
+    line = "".join(map(str, line))
+    line2 = line[::-1]
+    if line == line2:
+        return True
+    return False
+
 
 print(is_palindrome(input().strip()))
