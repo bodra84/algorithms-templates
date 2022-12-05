afd5f988d0b552d356581c5c126cbf9ec2cc054a
Bodra84 <Bodra84@mail.ru>
2022-12-05 21:47:59 +0300

    Задача Работа из дома

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,18 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    if number == 0:
+        return '0'
+    result = []
+    while number != 0:
+        if number % 2 == 1:
+            result.append(1)
+        else:
+            result.append(0)
+        number = number // 2
+    return "".join(map(str, result[::-1]))
+
 
 def read_input() -> int:
     return int(input().strip())
+
 
 print(to_binary(read_input()))
