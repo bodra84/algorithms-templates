89ea6c38905b6aeec669d173b237cc1b6ff6d003
Bodra84 <Bodra84@mail.ru>
2022-12-06 23:03:50 +0300

    Задача Двоичная система

--- python/sprint1_nonfinals/h.py
+++ python/sprint1_nonfinals/h.py
@@ -1,8 +1,25 @@
 from typing import Tuple
 
+
 def get_sum(first_number: str, second_number: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    width = max(len(first_number), len(second_number))
+    f = first_number.zfill(width)
+    s = second_number.zfill(width)
+    result = []
+    r = 0
+    for i in range(width-1, -1, -1):
+        t = int(f[i]) + int(s[i]) + r
+        if t > 1:
+            t -= 2
+            r = 1
+        else:
+            r = 0
+        result.append('1' if t else '0')
+    if r == 1:
+        result.append('1')
+    result.reverse()
+    return "".join(result)
+
 
 def read_input() -> Tuple[str, str]:
     first_number = input().strip()
@@ -10,4 +27,5 @@ def read_input() -> Tuple[str, str]:
     return first_number, second_number
 
 first_number, second_number = read_input()
+
 print(get_sum(first_number, second_number))
