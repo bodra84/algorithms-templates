9ae5f782e74992e1012ab225e85239d1e59ede8f
Bodra84 <Bodra84@mail.ru>
2022-12-06 23:44:37 +0300

    Задача Степень четырех

--- python/sprint1_nonfinals/i.py
+++ python/sprint1_nonfinals/i.py
@@ -1,5 +1,14 @@
 def is_power_of_four(number: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    if number in (1, 4):
+        return True
+    while number > 0:
+        c = number // 4
+        o = number % 4
+        if 2 < c <= 4 and o == 0:
+            return True
+        elif 0 < o < 4:
+            return False
+        number = c
+
 
 print(is_power_of_four(int(input())))
