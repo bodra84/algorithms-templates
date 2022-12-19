6a33f8c9cef949e0b90fb27d79ee7b46aaaa3963
Bodra84 <Bodra84@mail.ru>
2022-12-19 23:37:04 +0300

    Задача Дек

--- python/sprint2_Final/A_deque_0.py
+++ python/sprint2_Final/A_deque_0.py
@@ -1,6 +1,4 @@
-# 79765633 19 дек 2022, 23:33:57
 class Deque:
-    """Класс Дек."""
     def __init__(self, max_size):
         self.queue = [None] * max_size
         self.max = max_size
@@ -9,54 +7,50 @@ class Deque:
         self.size = 0
 
     def is_empty(self):
-        """Метод проверяет пуста ли очередь."""
         return self.size == 0
 
-    def push_back(self, value):
-        """Метод добавляет элемент в конец дека."""
+    def push_back(self, x):
         if self.size != self.max:
             while self.queue[self.tail]:
                 self.tail = (self.tail + 1) % self.max
-            self.queue[self.tail] = value
+            self.queue[self.tail] = x
             self.size += 1
         else:
             return 'error'
 
-    def push_front(self, value):
-        """Метод добавляет элемент в начало дека."""
+    def push_front(self, x):
         if self.size != self.max:
             while self.queue[self.head]:
                 self.head = (self.head - 1) % self.max
-            self.queue[self.head] = value
+            self.queue[self.head] = x
             self.size += 1
         else:
             return 'error'
 
     def pop_back(self):
-        """Метод возвращает последний элемент дека и удаляет его."""
         if self.is_empty():
             return print('error')
-        value = self.queue[self.tail]
+        x = self.queue[self.tail]
         self.queue[self.tail] = None
         if self.size > 1:
             self.tail = (self.tail-1) % self.max
         self.size -= 1
-        return value
+        return x
 
     def pop_front(self):
-        """Метод возвращает первый элемент дека и удаляет его."""
         if self.is_empty():
             return print('error')
-        value = self.queue[self.head]
+        x = self.queue[self.head]
         self.queue[self.head] = None
         if self.size > 1:
             self.head = (self.head + 1) % self.max
         self.size -= 1
-        return value
+        return x
 
 
 def read_input():
     """Функция ввода исходных данных."""
+    data = []
     n = int(input())
     size = int(input())
     data = ([input().split() for _ in range(n)])
@@ -68,15 +62,15 @@ stack_methods = {'push_back': Deque.push_back, 'pop_back': Deque.pop_back,
 
 
 def main():
-    max_size, data = read_input()
+    max_size, comands = read_input()
     q = Deque(max_size)
-    for command, *args in data:
-        try:
-            result = getattr(q, command)(*args)
-            if result:
-                print(result)
-        except AttributeError:
-            print("Ошибка команды!")
+    for comand in comands:
+        if len(comand) > 1:
+            m = stack_methods[comand[0]](q, comand[1])
+        else:
+            m = stack_methods[comand[0]](q)
+        if m:
+            print(m)
 
 
 if __name__ == '__main__':
